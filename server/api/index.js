e1300d594ad1913ba0d0bc22fc22e8301e4d6770
Hystee <hysteebyhystee@gmail.com>
2018-11-24 22:43:19 -0500

    feat(gifs): Combine python/upload to single route
    Add python function call to POST /upload api route, create new page entry with downloadURL of created gif associated with user
    
    #15

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
-router.use('/python', require('./python'))
+// router.use('/python', require('./python'))
 router.use('/upload', require('./upload'))
 router.use('/stickers', require('./stickers'))
 router.use('/page', require('./page'))
