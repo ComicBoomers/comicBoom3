0d00703ce85758c2302cb8147ccd9f43fa056b56
Jinyan <jinyan.shen21@gmail.com>
2018-11-28 16:52:28 -0500

    (style) css in progress

--- client/components/SinglePage.js
+++ client/components/SinglePage.js
@@ -51,52 +51,45 @@ class SinglePage extends React.Component {
     return (
       <div id="myPage">
         {this.props.myPage ? (
-          <div className="myPage">
-            <div />
+          <div>
+            <div className="myPage">
+              <img src={this.props.myPage.location} />
+            </div>
             {/* <h1 className=''>Comic Title</h1> */}
             {/* <button type="button">
               <Link to="/createComic">
-                Edit Comic
+              Edit Comic
               </Link>
             </button> */}
-            {this.state.areYouSure ? (
-              <button type="button" onClick={this.handleDelete}>
-                Confirm Delete
-              </button>
-            ) : (
-              <button type="button" onClick={this.triggerConfirm}>
-                Delete Comic
-              </button>
-            )}
-            <img src={this.props.myPage.location} />
+            <div>
+              {this.state.areYouSure ? (
+                <button type="button" onClick={this.handleDelete} className='delete' >
+                  Confirm Delete
+                </button>
+              ) : (
+                <button type="button" onClick={this.triggerConfirm} className='delete' >
+                  Delete Comic
+                </button>
+              )}
+            </div>     
             <h3 className='pageText'>Share It!</h3>
-            <FacebookShareButton url={shareUrl}>
-              <FacebookIcon
-              size={32}
-              round />
-            </FacebookShareButton>
-            <TwitterShareButton
-            url={shareUrl}>
-            <TwitterIcon
-              size={32}
-              round />
-            </TwitterShareButton>
-            <WhatsappShareButton
-              url={shareUrl}>
-              <WhatsappIcon size={32} round />
-            </WhatsappShareButton>
-            <TumblrShareButton
-            url={shareUrl}>
-            <TumblrIcon
-              size={32}
-              round />
-            </TumblrShareButton>
-            <EmailShareButton
-            url={shareUrl}>
-            <EmailIcon
-              size={32}
-              round />
-            </EmailShareButton>
+            <div className='shareButton'>
+              <FacebookShareButton url={shareUrl} >
+                <FacebookIcon size={32} round />
+              </FacebookShareButton>
+              <TwitterShareButton url={shareUrl} >
+                <TwitterIcon size={32} round />
+              </TwitterShareButton>
+              <WhatsappShareButton url={shareUrl} >
+                <WhatsappIcon size={32} round />
+              </WhatsappShareButton>
+              <TumblrShareButton url={shareUrl} >
+                <TumblrIcon size={32} round />
+              </TumblrShareButton>
+              <EmailShareButton url={shareUrl} >
+                <EmailIcon size={32} round />
+              </EmailShareButton>
+            </div>
           </div>
         ) : (
           <div />
