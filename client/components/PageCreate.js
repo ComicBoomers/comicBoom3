8ee5224c4eeadbdec01a6d8dedc5c1334f809973
Hystee <hysteebyhystee@gmail.com>
2018-11-28 13:00:21 -0500

    fix(sticker): Add default no-sticker option

--- client/components/PageCreate.js
+++ client/components/PageCreate.js
@@ -10,7 +10,7 @@ class PageCreate extends React.Component {
   constructor() {
     super()
     this.state = {
-      stickerId: '',
+      stickerId: '0',
       stickerX: null,
       stickerY: null,
       loading: false
@@ -19,7 +19,7 @@ class PageCreate extends React.Component {
     this.allowDrop = this.allowDrop.bind(this)
     this.drop = this.drop.bind(this)
     // this.mouseTracker = this.mouseTracker.bind(this)
-    this.savePage=this.savePage.bind(this)
+    this.savePage = this.savePage.bind(this)
   }
 
   allowDrop(e) {
@@ -31,24 +31,24 @@ class PageCreate extends React.Component {
     const id = this.props.stickerId
 
     //Document Relative: (mouse coords on Drop) - rounded to match save coords
-    const [mouseX, mouseY] = [Math.round(e.pageX),Math.round(e.pageY)]
-    console.log("drop event page x,y:", mouseX, mouseY)
+    const [mouseX, mouseY] = [Math.round(e.pageX), Math.round(e.pageY)]
+    console.log('drop event page x,y:', mouseX, mouseY)
 
     //Placing sticker at mouse drop coordinates
     const itm = document.getElementById(id)
 
     const stickerPlacement = (X, Y) => {
-        const clone = itm.cloneNode(true)
-        clone.style.position = "absolute"
-        clone.style.top = `${Y}px`
-        clone.style.left = `${X}px`
-        return clone
+      const clone = itm.cloneNode(true)
+      clone.style.position = 'absolute'
+      clone.style.top = `${Y}px`
+      clone.style.left = `${X}px`
+      return clone
     }
 
     e.target.append(stickerPlacement(mouseX, mouseY))
 
     //drop-zone coords (top, left corner)
-    const elem = document.getElementById("newPage")
+    const elem = document.getElementById('newPage')
     const coords = elem.getBoundingClientRect()
     const [elemX, elemY] = [coords.left + pageXOffset, coords.top + pageYOffset]
 
@@ -56,7 +56,6 @@ class PageCreate extends React.Component {
     const inHouseId = Number(id) + 1
     const [x, y] = [Math.round(mouseX - elemX), Math.round(mouseY - elemY)]
     this.setState({stickerId: `${inHouseId}`, stickerX: x, stickerY: y})
-
   }
 
   // mouseTracker(e) {
@@ -64,34 +63,31 @@ class PageCreate extends React.Component {
   // }
 
   async savePage() {
-  // for now will only merge 1 sticker to gif and save gif (gif saved via backend so upon response it will be in the database)
+    // for now will only merge 1 sticker to gif and save gif (gif saved via backend so upon response it will be in the database)
     this.setState({loading: true})
-    console.log("hit here",this.state)
+    console.log('hit here', this.state)
     const objToMerge = this.state
     console.log('state:', objToMerge)
     const res = await axios.put('/api/upload', objToMerge)
     if (res) {
       this.setState({loading: false})
       this.props.history.push('/')
-    } 
+    }
   }
 
   render() {
     const userId = this.props.user.id
-    console.log("USER ID: ", userId)
-    const pagePath = `/tmp/gifs/${userId}/temp.gif` 
+    console.log('USER ID: ', userId)
+    const pagePath = `/tmp/gifs/${userId}/temp.gif`
 
     const loading = this.state.loading
     //we need the user ID
     //page is going to be coming from temp/gifs/ not state...how to call that up ?/?
     //place ${page} where hard coded url is now
 
-    return (
-      
-       loading ? 
-        <Loading />
-      :
-      
+    return loading ? (
+      <Loading />
+    ) : (
       <div id="myPage">
         <span>
           <Stickers className="sidebar" />
@@ -102,7 +98,7 @@ class PageCreate extends React.Component {
             }}
             id="newPage"
             className="dropzone"
-            dropzone='copy'
+            dropzone="copy"
             onDragOver={this.allowDrop}
             onDrop={this.drop}
             // onMouseOver={this.mouseTracker}
@@ -110,11 +106,9 @@ class PageCreate extends React.Component {
         </span>
 
         <div>
-          {/* <Link to="/"> */}
-            <button type="button" onClick={this.savePage}>
-              SAVE
-            </button>
-          {/* </Link> */}
+          <button type="button" onClick={this.savePage}>
+            SAVE
+          </button>
         </div>
       </div>
     )
